a5579d761ec065bc5f0ba6ff209b79d8674a1154
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2020-04-14 09:08:50 +0300

    Redirect to login page if an unauthorized error occurred.

--- src/app/core/interceptors/auth-header-interceptor.ts
+++ src/app/core/interceptors/auth-header-interceptor.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthHeaderInterceptor implements HttpInterceptor {
+
+    constructor(private router: Router) { }
 
     intercept(
         request: HttpRequest<any>,
@@ -13,13 +17,24 @@ export class AuthHeaderInterceptor implements HttpInterceptor {
 
         const authToken = localStorage.getItem(environment.authTokenKey);
 
+        let requestWithAuthToken = request;
+
         if (authToken) {
-            const requestWithAuthToken = request.clone({
+            requestWithAuthToken = request.clone({
                 headers: request.headers.set('Authorization', `Bearer ${authToken}`)
             });
-            return next.handle(requestWithAuthToken);
         }
 
-        return next.handle(request);
+        return next.handle(requestWithAuthToken)
+            .pipe(tap(() => { },
+                (error: any) => {
+                    if (error instanceof HttpErrorResponse) {
+                        if (error.status !== 401) {
+                            return;
+                        }
+
+                        this.router.navigate(['/auth/login'], { queryParams: { returnUrl: this.router.url } });
+                    }
+                }));
     }
 }
